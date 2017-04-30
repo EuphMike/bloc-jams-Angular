41ee53e0d9e3bbea04487982a8d8c99a2c3734c0
Mike Waddell <Mike Waddell>
2017-04-30 17:44:05 -0400

    checkpoint11AngularUpdateComplete

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -60,21 +60,23 @@
                             notifyOnChange(scope.value);
                         });
                     });
-                 };
 
-                var notifyOnChange = function(newValue) {
-                    if (typeof scope.onChange === 'function') {
-                        scope.onChange({value: newValue});
-                    }
+                 $document.bind('mouseup.thumb', function() {
+                     $document.unbind('mousemove.thumb');
+                     $document.unbind('mouseup.thumb');
+
+                   });
                 };
 
-                    $document.bind('mouseup.thumb', function() {
-                        $document.unbind('mousemove.thumb');
-                        $document.unbind('mouseup.thumb');
-                    });
-              }
+              var notifyOnChange = function(newValue) {
+                  if (typeof scope.onChange === 'function') {
+                      scope.onChange({value: newValue});
+
+                   }
+               };
+            }
         };
-    }
+    };
 
     angular
         .module('blocJams')
